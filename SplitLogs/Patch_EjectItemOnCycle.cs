c6c9de6804aa5e1ebfdfb20fbc0fcd66d766a6d1
neronix17 <39186138+neronix17@users.noreply.github.com>
2024-05-31 00:38:04 +0100

    Initial commit

---
+++ SplitLogs/Patch_EjectItemOnCycle.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using HarmonyLib;
+using BepInEx.Configuration;
+
+namespace SplitLogs
+{
+    [HarmonyPatch(typeof(ItemDepositAndChanger), "ejectItemOnCycle")]
+    public static class Patch_EjectItemOnCycle
+    {
+		public static ConfigEntry<int> PlankQuantity { get; set; }
+
+		public static ConfigEntry<bool> CanSplitMetal { get; set; }
+
+		public static ConfigEntry<int> SheetQuantity { get; set; }
+
+		public static void Postfix(ItemDepositAndChanger __instance, int xPos, int yPos, HouseDetails inside = null)
+		{
+			int num = PlankQuantity.Value;
+			if (num < 0)
+			{
+				num = 2;
+			}
+			num--;
+			ItemChange itemChange = Inventory.Instance.allItems[(inside != null) ? inside.houseMapOnTileStatus[xPos, yPos] : WorldManager.Instance.onTileStatusMap[xPos, yPos]].itemChange;
+			ItemChangeType[] changesAndTheirChanger = itemChange.changesAndTheirChanger;
+			int num2 = ((inside != null) ? inside.houseMapOnTile[xPos, yPos] : WorldManager.Instance.onTileMap[xPos, yPos]);
+			ItemChangeType[] array = changesAndTheirChanger;
+			foreach (ItemChangeType val in array)
+			{
+				if (val.depositInto.tileObjectId == num2)
+				{
+					InventoryItem item = Inventory.Instance.allItems[inside != null ? inside.houseMapOnTileStatus[xPos, yPos] : WorldManager.Instance.onTileStatusMap[xPos, yPos]];
+					if (val.taskType == DailyTaskGenerator.genericTaskType.CutPlanks)
+					{
+						NetworkMapSharer.Instance.spawnAServerDropToSave(item.itemChange.getChangerResultId(inside != null ? inside.houseMapOnTile[xPos, yPos] : WorldManager.Instance.onTileMap[xPos, yPos]), num, __instance.ejectPos.position, inside, false, -1);
+						DailyTaskGenerator.generate.doATask(DailyTaskGenerator.genericTaskType.CutPlanks, num);
+						break;
+					}
+					else if (CanSplitMetal.Value && item.itemName == "Tin Bar")
+					{
+						NetworkMapSharer.Instance.spawnAServerDropToSave(item.itemChange.getChangerResultId(inside != null ? inside.houseMapOnTile[xPos, yPos] : WorldManager.Instance.onTileMap[xPos, yPos]), num, __instance.ejectPos.position, inside, false, -1);
+						break;
+					}
+				}
+			}
+		}
+	}
+}
---
+++ SplitLogs/SplitLogs.cs
@@ -0,0 +1,43 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SplitLogs
+{
+    [BepInPlugin("neronix17.dinkum.splitlogs", "Split Logs", "1.0.0")]
+    public class SplitLogs : BaseUnityPlugin
+	{
+		public const string pluginGuid = "neronix17.dinkum.splitlogs";
+
+		public const string pluginName = "Split Logs";
+
+		public const string pluginVersion = "1.0.0";
+
+		public ConfigEntry<int> PlankQuantity;
+
+		public ConfigEntry<bool> CanSplitMetal { get; set; }
+
+		public ConfigEntry<int> SheetQuantity { get; set; }
+
+		public void Awake()
+		{
+			Config.Bind("!Developer", "NexusID", 340, "Nexus Mod ID, automatically generated, changing it will do nothing.");
+			PlankQuantity = Config.Bind("Mod General", "Planks Quantity", 2, "Quantity of planks to drop when cutting a log on the table saw (including the one dropped by default)");
+			CanSplitMetal = Config.Bind("Mod General", "Can Split Metal", false, "If true, tin ingots can be split into multiple sheets.");
+			SheetQuantity = Config.Bind("Mod General", "Sheets Quantity", 2, "Quantity of sheets to drop when cutting a tin ingot on the table saw (including the one dropped by default)");
+			Logger.LogInfo(":: Split Logs :: Successfully Loaded ::");
+			Harmony harmony = new Harmony("neronix17.dinkum.splitlogs");
+			harmony.PatchAll();
+			Patch_EjectItemOnCycle.PlankQuantity = PlankQuantity;
+			Patch_EjectItemOnCycle.CanSplitMetal = CanSplitMetal;
+			Patch_EjectItemOnCycle.SheetQuantity = SheetQuantity;
+		}
+	}
+}
